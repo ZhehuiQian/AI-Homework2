5e1402809082e351a074d39d0ced8118866270d6
Zhehui Qian (RIT Student) <zq7880@ad.rit.edu>
2017-10-26 20:10:38 -0400

    follow the tutorial

---
+++ Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour {
+
+    public int width = 256;
+    public int height = 256;
+    public int scale = 20;
+
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    private void Start()
+    {
+        offsetX = Random.Range(0, 999999f);
+        offsetY = Random.Range(0, 999999f);
+    }
+
+    private void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+
+    }
+
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        // genreate perlin noise map fot the texture
+
+        for(int x=0;x<width;x++)
+        {
+            for(int y=0;y<height;y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float) x / width * scale+ offsetX;
+        float yCoord = (float) y / height * scale+ offsetY;
+        float sample= Mathf.PerlinNoise(xCoord, yCoord); // need write my own perlinnoise
+        return new Color (sample,sample,sample);
+    }
+}
---
+++ Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour {
+
+    public int width = 256;
+    public int height = 256;
+    public int depth = 20;
+    public float scale = 20;
+
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    // Use this for initialization
+    void Start () {
+        offsetX = Random.Range(0, 999999f);
+        offsetY = Random.Range(0, 999999f);
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+	}
+	
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for(int x=0;x<width;x++)
+        {
+            for(int y=0;y<height;y++)
+            {
+                heights[x,y] =CalculateHeight(x, y);
+            }
+        }
+        return heights;
+    }
+
+    float CalculateHeight(int x,int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord); // need write my own perlinnoise
+        return sample;
+    }
+}
